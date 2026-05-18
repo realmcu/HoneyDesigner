749991c0f86850873e7b6b9040932e67e0119777
Belief997 <xieyr997@gmail.com>
2026-05-18 17:37:12 +0800

    feat: 更新 gui lib

--- lib/sim/include/gui_version.h
+++ lib/sim/include/gui_version.h
@@ -1,11 +1,16 @@
+/*
+ * Copyright (c) 2026, Realtek Semiconductor Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ */
 
 #ifndef GUI_VERSION_H
 #define GUI_VERSION_H
 
 #define VERSION_TAG "v2.1.1.0"
-#define VERSION_BRANCH "master"
-#define VERSION_COMMIT "48fa6d1c1"
-#define VERSION_BUILD_DATE "2026-01-29 17:56:55"
+#define VERSION_BRANCH "HEAD"
+#define VERSION_COMMIT "feca4953f"
+#define VERSION_BUILD_DATE "2026-05-18 17:04:51"
 #define VERSION_REPO_STATUS "dirty"
 
 #endif // GUI_VERSION_H
