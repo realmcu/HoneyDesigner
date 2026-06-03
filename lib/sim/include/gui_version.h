b23cfbd00759effe66f27056d274ea9cabb18f34
shel_deng <shel_deng@realsil.com.cn>
2026-06-03 10:58:54 +0800

    feat: 更新lib

--- lib/sim/include/gui_version.h
+++ lib/sim/include/gui_version.h
@@ -8,9 +8,9 @@
 #define GUI_VERSION_H
 
 #define VERSION_TAG "v2.1.1.0"
-#define VERSION_BRANCH "HEAD"
-#define VERSION_COMMIT "00759894b"
-#define VERSION_BUILD_DATE "2026-06-01 16:40:58"
+#define VERSION_BRANCH "master"
+#define VERSION_COMMIT "b4c4cc033"
+#define VERSION_BUILD_DATE "2026-06-02 11:42:51"
 #define VERSION_REPO_STATUS "dirty"
 
 #endif // GUI_VERSION_H
